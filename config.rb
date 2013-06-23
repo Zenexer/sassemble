fa87a43775b5fe074757efd757f0ec9306fdf8d8
Paul Buonopane (Zenexer) <paul+git@earth2me.com>
2013-06-23 02:42:35 -0400

    Configured Compass/Sass.

--- config.rb
+++ config.rb
@@ -1,24 +1,30 @@
-# Require any additional compass plugins here.
+# Plugins
+# End Plugins
+
+# Note that these settings only affect development preview code, and have no effect
+# on the output of implementing applications.  Therefore, these settings are
+# optimized for debugging.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+sass_dir = 'sass'
+css_dir = 'tmp/css'
+images_dir = 'tmp/images'
+javascripts_dir = 'tmp/js'
 
-# You can select your preferred output style here (can be overridden via the command line):
+# Preferred output style; can be overridden via command line.
 # output_style = :expanded or :nested or :compact or :compressed
+# 	:expanded
+# 	:nested
+# 	:compact
+# 	:compressed
+output_style = :nested
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+# Enable relative paths to assets via compass helper functions.
+relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# Enable ebugging comments that display the original location of your selectors.
+line_comments = true
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# We use purely SCSS.  Original-style Sass code is forbidden in this framework.
+preferred_syntax = :scss
